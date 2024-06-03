737cd8506f5b4c22f335f82cd7bbceebbd5dcc41
mjay1954 <mosesjoshua1954@gmail.com>
2024-06-03 12:32:56 +0100

    firstcommit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(' this is cloud computing DEVOPS ; CI-CD PIPELINE !'));
+app.get('/', (req, res) => res.send(' THIS IS CSC 314 CLOUD COMPUTING DEVOPS ; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS.  MOSES JOSHUA OZIOMA VUG/CSC/22/8257'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
